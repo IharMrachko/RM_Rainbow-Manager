18b8e917b9a4196d2150984ae760ff0523f3b20d
Igor Mrochko <I.Mrochko@electrasoft.ru>
2025-09-18 13:29:57 +0300

    create login page and components: input, button, add icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'modern-css-reset';
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).use(store).use(router).mount('#app')
+// Добавляем иконку в библиотеку
+library.add(faEnvelope);
+library.add(faLock);
+
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<template>
+  <app-login></app-login>
+</template>
+<script setup lang="ts">
+import AppLogin from '@/components/AppLogin.vue';
+</script>
+<style scoped lang="scss"></style>
