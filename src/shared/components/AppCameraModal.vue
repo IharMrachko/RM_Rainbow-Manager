9856264329f0205af1ed4320f2afe70e6d9a649e
Igor Mrochko <I.Mrochko@electrasoft.ru>
2025-11-19 16:02:21 +0300

    fix16

--- src/shared/components/AppCameraModal.vue
+++ src/shared/components/AppCameraModal.vue
@@ -32,7 +32,7 @@
 
       <AppShutterButton @capture="takePhotoWithFrameMasked"></AppShutterButton>
       <div ref="modalRef" class="settings" @click="openOverlayPanel">
-        И9
+        И10
         <font-awesome-icon size="xl" :icon="['fas', 'sliders']" />
       </div>
     </footer>
@@ -202,7 +202,7 @@ export default defineComponent({
       }
 
       // apply zoom (scale) and optional pan offsets
-      const zoom = isMobile.value ? 2.1 : 1.2; // пример scale
+      const zoom = isMobile.value ? 2.2 : 1.2; // пример scale
       const offsetX = 0; // -0.5..+0.5
       const offsetY = 0; // -0.5..+0.5
 
@@ -356,7 +356,7 @@ footer {
 
 video {
   @media (max-width: 600px) {
-    height: 90%;
+    height: 60%;
   }
 }
 </style>
