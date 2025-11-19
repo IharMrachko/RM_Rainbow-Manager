97106c100c99537d0e3d830f17765fd97ec930b5
Igor Mrochko <I.Mrochko@electrasoft.ru>
2025-11-19 16:56:21 +0300

    fix22

--- src/shared/components/AppCameraModal.vue
+++ src/shared/components/AppCameraModal.vue
@@ -32,7 +32,7 @@
 
       <AppShutterButton @capture="takePhotoWithFrameMasked"></AppShutterButton>
       <div ref="modalRef" class="settings" @click="openOverlayPanel">
-        И16
+        И17
         <font-awesome-icon size="xl" :icon="['fas', 'sliders']" />
       </div>
     </footer>
@@ -117,7 +117,7 @@ export default defineComponent({
       // параметры в CSS-пикселях
       const padding = Math.max(6, Math.round(Math.min(cssW, cssH) * 0.02));
       const thickness = isMobile.value ? 20 : 80; // для компа 80;
-      const overSize = isMobile.value ? 1 : 2; // для компа 2
+      const overSize = isMobile.value ? 0.8 : 2; // для компа 2
       const cx = cssW / 2;
       const cy = cssH / 2;
       const radius = Math.min(cssW, cssH) / 2 - padding - thickness / overSize;
