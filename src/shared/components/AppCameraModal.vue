a552ed8609928f08ac6f286d40633c0c6e7c6ff0
Igor Mrochko <I.Mrochko@electrasoft.ru>
2025-12-09 21:03:20 +0300

    test safari  camera

--- src/shared/components/AppCameraModal.vue
+++ src/shared/components/AppCameraModal.vue
@@ -319,7 +319,7 @@ export default defineComponent({
       //flex: 1; /* занимает всё оставшееся пространство */
       position: relative;
       width: 100%;
-      height: 60%;
+      height: 65%;
     }
   }
 }
