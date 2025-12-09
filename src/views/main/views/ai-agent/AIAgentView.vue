417aaaea5b52e6f4f7e37927d212e1162fbcca45
Igor Mrochko <I.Mrochko@electrasoft.ru>
2025-12-09 17:20:32 +0300

    test safari  ai agent

--- src/views/main/views/ai-agent/AIAgentView.vue
+++ src/views/main/views/ai-agent/AIAgentView.vue
@@ -199,7 +199,7 @@ export default defineComponent({
     display: flex;
     align-items: center;
     justify-content: center;
-    bottom: 0;
+    bottom: 60px;
     background: var(--color-wrap-bg);
 
     @media (max-width: 600px) {
