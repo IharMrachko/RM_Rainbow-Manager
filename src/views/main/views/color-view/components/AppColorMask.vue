40264c0b1781251e448f108b2079444a5dbd9839
Igor Mrochko <I.Mrochko@electrasoft.ru>
2025-12-11 12:01:07 +0300

    fix ios

--- src/views/main/views/color-view/components/AppColorMask.vue
+++ src/views/main/views/color-view/components/AppColorMask.vue
@@ -219,7 +219,7 @@ export default defineComponent({
         currentUserId: currentUser.value?.uid,
         canvas: editorCanvasRef.value?.getCanvasValue(),
         imageUrl: imageUrl.value,
-      });
+      }).then(() => window.scrollTo(0, 0));
     };
     const openImageSettingsModal = async () => {
       visiblePopover.value = false;
@@ -301,7 +301,7 @@ export default defineComponent({
   width: 100%;
   flex-wrap: wrap;
   background: var(--color-wrap-bg);
-  height: calc(100dvh - var(--header-height));
+  height: calc(100dvh - var(--header-height) - var(--tabs-height-with-padding));
   gap: 20px;
 
   & .selected {
