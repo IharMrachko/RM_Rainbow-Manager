ccbf3f5e5ce640ec321a7fddbae38a89afa05a4d
Igor Mrochko <I.Mrochko@electrasoft.ru>
2025-12-12 13:08:53 +0300

    fix ios

--- src/views/open/views/SignInView.vue
+++ src/views/open/views/SignInView.vue
@@ -3,7 +3,7 @@
     <div class="wrap-login">
       <app-image-login></app-image-login>
       <div class="login-form">
-        <h1 class="title">{{ t('login') }}</h1>
+        <!--        <h1 class="title">{{ t('login') }}</h1>-->
         <VForm :validation-schema="formGroup" @submit="onSubmit">
           <section class="login-inputs">
             <Field v-slot="{ field, meta, errorMessage }" name="email">
@@ -172,7 +172,7 @@ export default defineComponent({
     border-radius: 0;
     margin-top: 0;
     padding: 1.5rem;
-    height: calc(100vh - 76px);
+    height: 100dvh;
   }
 }
 
@@ -185,7 +185,6 @@ export default defineComponent({
   @media (max-width: 600px) {
     width: 100%;
     gap: 1rem;
-    flex: 0 0 0;
   }
 }
 
