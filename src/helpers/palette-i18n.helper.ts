9d5172283b942d5356eca5e73d93708b3072cf70
Igor Mrochko <I.Mrochko@electrasoft.ru>
2026-01-28 17:20:24 +0300

    fix translate

--- src/helpers/palette-i18n.helper.ts
+++ src/helpers/palette-i18n.helper.ts
@@ -3,7 +3,7 @@ import { Palette } from '@/types/palette.type';
 export const paletteI18nHelper: Map<Palette, string> = new Map<Palette, string>([
   ['softSummerPalette', 'softSummerPaletteShort'],
   ['darkAutumnPalette', 'darkAutumnPaletteShort'],
-  ['coldSummerPalette', 'coolSummerPaletteShort'],
+  ['coldSummerPalette', 'coldSummerPaletteShort'],
   ['lightSpringPalette', 'lightSpringPaletteShort'],
   ['softAutumnPalette', 'softAutumnPaletteShort'],
   ['brightSpringPalette', 'brightSpringPaletteShort'],
