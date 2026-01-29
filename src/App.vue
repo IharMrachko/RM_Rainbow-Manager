ab86bf6b23c8638f0189a9475191515114d0a91c
Igor Mrochko <I.Mrochko@electrasoft.ru>
2026-01-29 12:22:32 +0300

    refactor navigation

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,6 @@ import AppToaster from '@/shared/components/AppToaster.vue';
 import AppDialog from '@/shared/components/dialog/AppDialog.vue';
 
 import { Device } from '@/store/modules/mobile-view';
-import { useRouter } from 'vue-router';
 
 export default defineComponent({
   components: { AppDialog, AppToaster },
@@ -20,8 +19,6 @@ export default defineComponent({
     const windowWidth = ref(document.documentElement.clientWidth);
     const store = useStore();
     const { locale } = useI18n();
-    const router = useRouter();
-    const isAuthenticated = store.getters['authFirebase/isAuthenticated'];
     store.dispatch('theme/initTheme');
     store.dispatch('language/initLanguage');
     const device = ref<Device>('desktop');
@@ -32,9 +29,6 @@ export default defineComponent({
       else if (/android/.test(ua)) device.value = 'android';
       else device.value = 'desktop';
       store.dispatch('mobile/setDevice', { device: device.value });
-      if (isAuthenticated) {
-        router.push('main');
-      }
     });
 
     watch(
